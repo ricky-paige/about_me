28a5f7257c2b2df46bf9249d5eaa32e49f6fec61
ricky-paige <rickyofthe9@gmail.com>
2016-11-29 14:59:04 -0800

    renamed var to response 1

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
-var question1 = prompt('Does Ricky have any pets?').toLowerCase();
+var response1 = prompt('Does Ricky have any pets?').toLowerCase();
 
-if (question1 === 'no' || question1 === 'n') {
+if (response1 === 'no' || response1 === 'n') {
   alert('Correct! Good job my dude');
   console.log('The user is right!');
   document.write('1. You got it right!');
-} else if (question1 === 'yes' || question1 === 'y') {
-  alert('You got it wrong friend');
+} else if (response1 === 'yes' || response1 === 'y') {
+  alert('1. You got it wrong friend');
   console.log('The user is wrong');
   document.write('Better luck next time /:');
 } else {
-  alert('That\'s not even a choice are you dumb?');
+  alert('1. That\'s not even a choice friendo');
   console.log('The user is not smart.');
-  document.write('Did you even try?');
+  document.write('Try again :)');
 }
